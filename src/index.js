4c155161749e933237fe2b18aaf096ee0969f3b5
Phile105 <163855550+Phile105@users.noreply.github.com>
2024-05-14 16:56:26 +0200

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,41 @@
+function search(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector("#search-input");
+  let cityName = document.querySelector(".current-city");
+  cityName.innerHTML = searchInput.value;
+}
+
+function formatDate(date) {
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+  let day = date.getDay();
+
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Suterday",
+  ];
+
+  let formattedDay = days[day];
+  return `${formattedDay} ${hours}:${minutes}`;
+}
+
+let searchForm = document.querySelector("#search-form");
+searchForm.addEventListener("submit", search);
+
+let currentDateElement = document.querySelector("#current-date");
+let currentDate = new Date();
+
+currentDateElement.innerHTML = formatDate(currentDate);
